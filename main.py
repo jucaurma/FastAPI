1953cf9dadab174081570439d3607dc9d7ca2528
jucaurma <155864978+jucaurma@users.noreply.github.com>
2024-10-03 18:52:10 +0200

    Inicio de repositorio

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get("/")
 async def hello_world():
     return{"message": "Hello World!"}
+
+
+#Ruta para saludo personalizado
+@app.get("/saludo/{nombre}")
+async def saludo_personalizado(nombre:str):
+    return {"message": f"¡Hola, {nombre}!"}
