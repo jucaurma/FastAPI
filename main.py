6b9d9599be7773528f3f52e502005386e80a79ad
jucaurma <155864978+jucaurma@users.noreply.github.com>
2024-10-08 18:15:19 +0200

    Uso de Modelos en FastAPI

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Body
+from typing import Union, Optional
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -11,3 +13,59 @@ async def hello_world():
 @app.get("/saludo/{nombre}")
 async def saludo_personalizado(nombre:str):
     return {"message": f"¡Hola, {nombre}!"}
+
+
+@app.get("/todo1")
+async def get_all():
+    return [
+        {"id":1, "description": "Learn Python", "complete": True},
+         {"id":2, "description": "Learn Fast API", "complete": False},
+    ]
+
+
+TODO_LIST = [
+    {"id":1, "description": "Learn Python", "complete": True},
+    {"id":2, "description": "Learn Fast API", "complete": False},
+    {"id":3, "description": "Tarea 3", "complete": False},
+]
+
+
+class Todo(BaseModel):
+    id: Optional[int] = None
+    description: str = Field(min_length=5, max_length=500)
+    complete: bool = Field(default=False)
+
+
+
+@app.get("/todo2")
+async def get_all(complete: Union[bool, None] = None):
+    if complete is not None:
+        filtered_todos = list(filter(lambda todo: todo["complete"] == complete, TODO_LIST))
+        return filtered_todos
+    return TODO_LIST
+
+
+@app.get("/todo2/{todo_id}")
+async def get_todo(todo_id: int):
+    try:
+        todo_data = next(todo for todo in TODO_LIST if todo["id"] == todo_id )
+        return todo_data
+    except:
+        raise HTTPException(status_code=404, detail="TODO no found")
+    
+
+@app.post("/todo1")
+async def create_todo(id: int = Body(), description: str = Body(), complete: bool = Body()):
+    TODO_LIST.append({
+        "id": id,
+        "description": description,
+        "complete": complete
+    })
+    return TODO_LIST
+
+
+@app.post("/todo2")
+async def create_todo(data: Todo):
+    TODO_LIST.append(data)
+    return data
+
